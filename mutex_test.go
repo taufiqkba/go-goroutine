bfdfafee5c14673a9aa3b56041171a997dc7d7d2
Taufiq Kurniawan <111201811567@mhs.dinus.ac.id>
2022-09-28 14:53:30 +0700

    Learning mutex

---
+++ mutex_test.go
@@ -0,0 +1,59 @@
+package go_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.balance = account.balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestReadWriteMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+}
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Value X with mutex:", x)
+
+}
